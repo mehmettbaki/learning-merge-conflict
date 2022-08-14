55151dc89ffbc2d3310f6584836580de902d47a0
Mehmet BAKİ <43927054+mehmettbaki@users.noreply.github.com>
2022-08-14 22:24:03 +0300

    Update yeni.js

--- yeni.js
+++ yeni.js
@@ -1,5 +1,19 @@
-yeni dosyam 
+burasi biraz karisti
 
+
+b dosya clone komutundan sonra yaratilmistir
+yeni dosyam 
+yeni seyler eklendi
+
+
+b dosya clone komutundan sonra yaratilmistir
+yeni dosyam 
+yeni seyler eklendi
+
+
+b dosya clone komutundan sonra yaratilmistir
+yeni dosyam 
+yeni seyler eklendi
 
 
 b dosya clone komutundan sonra yaratilmistir
