7bac1385dc2b262fcb3e1a9e5a24b839acdc520f
Mehmet BAKİ <43927054+mehmettbaki@users.noreply.github.com>
2022-08-14 21:45:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-my main branch
+myyy mainnsd branch has got new
 
 
 const something = async () =>{
-
+    console.log('my main branch got update')
     return 'my main branch'
+}
+
+
+const somethingnew= async () ={
+    
+    
+    asdfasdfdsf
+    
+    
+    
+}
+
+const somethingnewthird= async () ={
+    
+    
+    third
+    
+    
+    
 }
